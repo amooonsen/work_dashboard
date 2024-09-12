86e329d819d0b7fbc2aa02a29a4708ba52528630
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-09-12 13:53:45 +0900

    컴포넌트 추가

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,13 @@
 import {Inter} from "next/font/google";
 import {Lusitana} from "next/font/google";
+import {Noto_Sans_KR} from "next/font/google";
 
 export const inter = Inter({subsets: ["latin"]});
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
+});
+export const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "400", "700", "900"],
 });
